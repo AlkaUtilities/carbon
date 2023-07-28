27817afb1422aa4282ddf93b0eaf24f88932615b
AldenizenMC <63831735+AldenizenMC@users.noreply.github.com>
2023-07-28 17:57:59 +0700

    Updated build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
+# Does the same thing as build.ps1 except in Bash (for Linux/macOS)
 
 buildPath="dist"
 srcPath="src"
 
-set -e
+if [ -d "$buildPath" ]; then
+    rm -rf "$buildPath"
+fi
 
-rm -rf $buildPath
-mkdir -p $buildPath
+if [ ! -d "$buildPath" ]; then
+    mkdir "$buildPath"
+fi
 
-ENTRYPOINTS=$(find -type f -name '*.[tj]s' -not -path './node_modules/*')
+ENTRYPOINTS=$(find "$srcPath" -type f \( -name "*.ts" -o -name "*.js" \) ! -path "*node_modules*")
 
-esbuild $ENTRYPOINTS --log-level=warning --outdir=$buildPath --outbase=$srcPath --sourcemap --target='node16' --platform='node' --format='cjs'
+npx esbuild $ENTRYPOINTS --log-level=warning --outdir="$buildPath" --outbase="$srcPath" --sourcemap --target="node16" --platform="node" --format="cjs"
 
-cp -R $srcPath/views $buildPath
-cp -R $srcPath/public $buildPath
+cp "$srcPath/config.yaml" "$buildPath"
+cp -r "$srcPath/views" "$buildPath"
+cp -r "$srcPath/public" "$buildPath"
