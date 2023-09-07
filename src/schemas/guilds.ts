d497349658d6b78cada977272cde68b20ecdfe27
AldenizenMC <63831735+AldenizenMC@users.noreply.github.com>
2023-09-07 22:29:11 +0700

    ACTUALLY made properties with typeof object required (nested object) for guilds.ts

--- src/schemas/guilds.ts
+++ src/schemas/guilds.ts
@@ -7,7 +7,7 @@ export const schema = new Schema({
         required: true,
         type: {
             // Role given to user when user passed verification, etc.
-            MainRoleID: { type: String, required: true, default: "000000000000000000" },
+            MainRoleID: { type: String, required: true },
             Modules: {
                 required: true,
                 type: {
@@ -15,26 +15,91 @@ export const schema = new Schema({
                         required: true,
                         type: {
                             Captcha: {
-                                Enabled: { type: Boolean, required: true, default: false },
+                                Enabled: { type: Boolean, required: true },
                                 MaxAttempts: { type: Number, required: true, default: 3 },
                             },
                             AccountAge: {
-                                Enabled: { type: Boolean, required: true, default: false },
-                                MinimumAccountAge: { type: Number, required: true, default: 7 }, // in days
+                                Enabled: { type: Boolean, required: true },
+                                MinimumAccountAge: { type: Number, required: true }, // in days
                                 Action: { type: String, required: true, default: "nothing" },
                             },
                             CheckResult: {
-                                Enabled: { type: Boolean, required: true, default: false },
-                                ChannelID: { type: String, required: true, default: "000000000000000000" }
+                                Enabled: { type: Boolean, required: true },
+                                ChannelID: { type: String, required: true }
                             },
                             LogUser: {
-                                Enabled: { type: Boolean, required: true, default: false },
-                                ChannelID: { type: String, required: true, default: "000000000000000000" }
+                                Enabled: { type: Boolean, required: true },
+                                ChannelID: { type: String, required: true }
                             }
                         }
                     }
                 },
             },
+            RateLimits: {
+                required: true,
+                type: {
+                    Global: {
+                        required: true,
+                        type: {
+                            Limit: { type: Number, required: true },
+                            Window: { type: Number, required: true },
+                        }
+                    },
+
+                    Channel: {
+                        required: true,
+                        type: {
+                            Create: {
+                                required: true,
+                                type: {
+                                    Limit: { type: Number, required: true },
+                                    Window: { type: Number, required: true }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        default: {
+            MainRoleID: "000000000000000000",
+            Modules: {
+                JoinGate: {
+                    Captcha: {
+                        Enabled: false,
+                        MaxAttempts: 3
+                    },
+
+                    AccountAge:{
+                        Enabled: false,
+                        MinimumAccountAge: 7,
+                        Action: "nothing"
+                    },
+
+                    CheckResult: {
+                        Enabled: false, 
+                        ChannelID: "000000000000000000"
+                    },
+
+                    LogUser: {
+                        Enabled: false,
+                        ChannelID: "000000000000000000"
+                    }
+                },
+            },
+            RateLimits: {
+                Global: {
+                    Limit: 5,
+                    Window: 60
+                },
+
+                Channel: {
+                    Create: {
+                        Limit: 5,
+                        Window: 60
+                    }
+                }
+            }
         }
     },
 })
